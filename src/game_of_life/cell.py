4298986687a4738da73e82b29f9f4a85769571f6
Ivan Covach <eeiprex@gmail.com>
2023-04-24 23:52:32 +0300

    [feature] implemente the Cell class

--- src/game_of_life/cell.py
+++ src/game_of_life/cell.py
@@ -1,20 +1,37 @@
+from typing import Sequence
+
+
 class Cell:
-    def __init__(self) -> None:
-        pass
+    __slots__ = ('__x', '__y', 'alive')
+    alive: bool
+    __x: int
+    __y: int
+
+    def __init__(self, coord: Sequence[int], alive: bool = False) -> None:
+        assert len(coord) == 2
+        self.alive = alive
+        self.__x, self.__y = coord
 
     def __str__(self) -> str:
-        return ''
+        return f'Cell(alive={self.alive} coord={self.coord})'
 
-    def is_alive(self) -> None:
+    @property
+    def coord(self) -> tuple[int, int]:
+        """Property for taking __x and __y"""
+        return self.__x, self.__y
+
+    def is_alive(self) -> bool:
         """Returns the state of life of the cell.
 
         Returns:
             Status is the cell alive or not
         """
+        return self.alive
 
-    def copy(self) -> None:
+    def copy(self) -> 'Cell':
         """Copies itself, creating a new cell
 
         Returns:
             Copy of the instance
         """
+        return Cell(coord=self.coord, alive=self.alive)
