9f33538bc0973607071171de0dd028784e6b21fb
Ivan Covach <eeiprex@gmail.com>
2023-04-24 23:42:29 +0300

    [chore] add Cell class in cell.py

---
+++ src/game_of_life/cell.py
@@ -0,0 +1,20 @@
+class Cell:
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return ''
+
+    def is_alive(self) -> None:
+        """Returns the state of life of the cell.
+
+        Returns:
+            Status is the cell alive or not
+        """
+
+    def copy(self) -> None:
+        """Copies itself, creating a new cell
+
+        Returns:
+            Copy of the instance
+        """
