a1e55043d5dd853cd761fc5ac95cae916346dbf5
Ivan Covach <eeiprex@gmail.com>
2023-04-25 16:44:12 +0300

    [feature] implemente next_cycle() method

--- src/game_of_life/life.py
+++ src/game_of_life/life.py
@@ -6,6 +6,7 @@ import config as c
 
 from app import App
 from cell import Cell
+from utils import quick_copy
 
 RowCell: TypeAlias = List[Cell]
 MatrixCell: TypeAlias = List[RowCell]
@@ -16,14 +17,14 @@ class GameEngine:
     app: App
     screen: pg.Surface
     color_cell: c.Color
-    previous_area: MatrixCell | None  # previous state of area
+    previous_area: MatrixCell  # previous state of area
     area: MatrixCell  # current state of area
 
     def __init__(self, app: App, screen: pg.Surface) -> None:
         self.app = app
         self.screen = screen
         self.color_cell = c.COLOR_CELL
-        self.previous_area = None
+        self.previous_area = [[]]
 
         width_x, height_y = self.app.width // c.CELL_SIZE, self.app.height // c.CELL_SIZE
 
@@ -61,3 +62,28 @@ class GameEngine:
         Returns:
             None
         """
+        self.previous_area = quick_copy(self.area)
+        for line in self.previous_area[1:-1]:
+            for cell in line[1:-1]:
+                number_living = 0
+                X, Y = cell.coord
+
+                for dx, dy in ((-1, 1), (0, 1), (1, 1), (1, 0)):
+                    column = Y + dy if Y + dy > 0 else 0
+                    row = X + dx if X + dx > 0 else 0
+
+                    number_living += self.previous_area[column][row].is_alive()
+
+                    column = Y - dy if Y - dy > 0 else 0
+                    row = X - dx if X - dx > 0 else 0
+
+                    number_living += self.previous_area[column][row].is_alive()
+
+                if cell.is_alive():
+                    if number_living not in (2, 3):
+                        x, y = cell.coord
+                        self.area[x][y].alive = False
+                else:
+                    if number_living == 3:
+                        x, y = cell.coord
+                        self.area[x][y].alive = True
