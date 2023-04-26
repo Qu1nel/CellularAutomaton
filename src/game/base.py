66c1e861bfaed8b57be9f6af389d795d047c2350
Ivan Covach <eeiprex@gmail.com>
2023-04-26 13:34:27 +0300

    [chore] Add new abstract class AppBase
    Update files:
    
        * base.py

--- src/game/base.py
+++ src/game/base.py
@@ -1,8 +1,26 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class AppBase(ABC):
-    pass
+    @abstractmethod
+    def handle_events(self) -> None:
+        pass
+
+    @abstractmethod
+    def draw(self) -> None:
+        pass
+
+    @abstractmethod
+    def process(self) -> None:
+        pass
+
+    @abstractmethod
+    def loop(self) -> None:
+        pass
+
+    @abstractmethod
+    def run(self) -> None:
+        pass
 
 
 class CellBase(ABC):
