c5feb2c933334a72c2d03229af85177520bdcebb
Ivan Covach <eeiprex@gmail.com>
2023-04-27 18:38:00 +0300

    [chore] Update version app to 0.6b

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 __author__ = "Qu1nel"
-__version__ = 0.5
+__version__ = 0.6
 __package__ = 'GameOfLife'
 __license__ = Path(__file__).parent.with_name("LICENSE").read_text(encoding='UTF-8')
 __doc__ = Path(__file__).parent.with_name("README.md").read_text(encoding='UTF-8')
