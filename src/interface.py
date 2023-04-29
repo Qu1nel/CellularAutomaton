3f8952a247c03c5b2e1338db80367ea9b64d3235
Ivan Covach <eeiprex@gmail.com>
2023-04-29 16:29:35 +0300

    [chore] Add enum Colors in interface.py
    Changes:
        change: [add]   src/interface.py

--- src/interface.py
+++ src/interface.py
@@ -1,10 +1,19 @@
 from typing import Tuple, Union, TypeAlias
+from enum import Enum
 
 import pygame as pg
 
 from base import InterfaceBase, RectBase, Buttons
 
 Number: TypeAlias = Union[int, float]
+
+
+class Colors(Enum):
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
 
 
 class Rect(RectBase):
