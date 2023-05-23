e70021609ccbaf26a0584ba0446a1dd529a9fd16
Ivan Covach <eeiprex@gmail.com>
2023-05-23 15:07:27 +0300

    [chore:meta] Update version
    Files:
        [ch] __init__.py
    
    Changes:
        Changes __version__ var to 1.0

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 __author__ = "Qu1nel"
-__version__ = 0.6
+__version__ = 1.0
 __package__ = 'GameOfLife'
 __license__ = Path(__file__).parent.with_name("LICENSE").read_text(encoding='UTF-8')
 __doc__ = Path(__file__).parent.with_name("README.md").read_text(encoding='UTF-8')
